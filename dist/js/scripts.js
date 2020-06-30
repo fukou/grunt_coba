ce15d7d31dd72726fd24c416811076db8678cb3d
Faiz Ichsan Jaya <fukuo@gmail.com>
2020-06-30 18:19:44 +0700

    first commit

--- dist/js/scripts.js
+++
@@ -1,9 +0,0 @@
-// file pertama untuk playground
-const hello = (name) => {
-    console.log("My name is ${name}");
-}
-
-hello("Faiz Ichsan Jaya");
-// file kedua
-console.error("This is a mistake");
-// file ketiga
